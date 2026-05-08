85d8246be91b69a8bba244298266e5db5031ff75
gazi04 <gazmendhalili2016@gmail.com>
2026-05-08 20:23:54 +0200

    :sparkles: Added one hot to encode the labels

--- activation_functions.py
+++ activation_functions.py
@@ -1,4 +1,4 @@
-from numpy import exp, maximum, ndarray
+from numpy import arange, exp, maximum, ndarray, zeros
 
 def relu(z):
     return maximum(0, z)
@@ -11,3 +11,9 @@ def relu_derivative(z: ndarray):
 
 def softmax(z: ndarray):
     return exp(z) / sum(exp(z))
+
+def one_hot(Y: ndarray) -> ndarray:
+    one_hot_Y = zeros((Y.size, Y.max() + 1))
+    one_hot_Y[arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
