7fffc73ff1fceb9097c6241a8c0ffdec5e4958cd
gazi04 <gazmendhalili2016@gmail.com>
2026-05-14 15:23:37 +0200

    :sparkles: Added backward propagation and update parameter method

--- main.py
+++ main.py
@@ -32,5 +32,23 @@ def calculate_loss(predictions: ndarray, results: ndarray):
     results = one_hot(results)
     return -(sum(results) * log(predictions))
 
+def backward_propagation(z1: ndarray, a1: ndarray, a2: ndarray, w2: ndarray, x: ndarray, y: ndarray, m: int):
+    dz2 = a2 - y # the error term of the output layer
+    dw2 = (1/m) * dz2.dot(a1.T) # weighted gradient for layer 2 (output layer)
+    db2 = (1/m) * sum(dz2, axis=1, keepdims=True) # bias gradient for layer 2 
+
+    dz1 = w2.T.dot(dz2) * relu_derivative(z1)
+    dw1 = (1/m) * dz1.dot(x.T)
+    db1 = (1/m) * sum(dz1, axis=1, keepdims=True)
+
+    return dw1, db1, dw2, db2
+
+def update_parameters(w1, w2, b1, b2, dw1, db1, dw2, db2, alpha):
+    w2 = w2 - (dw2*alpha)
+    b2 = b2 - (db2*alpha)
+    w1 = w1 - (dw1*alpha)
+    b1 = b1 - (db1*alpha)
+
+    return w1, w2, b1, b2
 if __name__ == "__main__":
     main()
